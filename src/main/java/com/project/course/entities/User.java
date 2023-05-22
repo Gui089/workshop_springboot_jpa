aef623d0f83611d3c045458c8350561d3a7c2f7f
Gui089 <guilhermesilba811@gmail.com>
2023-05-22 08:51:45 -0300

    H2 database created

--- src/main/java/com/project/course/entities/User.java
+++ src/main/java/com/project/course/entities/User.java
@@ -1,12 +1,19 @@
 package com.project.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     private static final Long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
