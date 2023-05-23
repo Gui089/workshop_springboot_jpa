b9cbfb49a1cd88724880ab5b117799818798b057
Gui089 <guilhermesilba811@gmail.com>
2023-05-23 08:16:24 -0300

    Order class

--- src/main/java/com/project/course/entities/User.java
+++ src/main/java/com/project/course/entities/User.java
@@ -3,6 +3,8 @@ package com.project.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -19,6 +21,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){
     }
@@ -70,7 +75,9 @@ public class User implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
-
+    public List<Order> getOrders() {
+        return orders;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -82,4 +89,6 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hash(getId());
     }
+
+
 }
