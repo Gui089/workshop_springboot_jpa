5d61eb026a2f7aa6d0374a9d9255764710305552
Gui089 <guilhermesilba811@gmail.com>
2023-05-24 08:19:24 -0300

    Many-to-many

--- src/main/java/com/project/course/entities/Category.java
+++ src/main/java/com/project/course/entities/Category.java
@@ -1,5 +1,6 @@
 package com.project.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,7 +18,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
