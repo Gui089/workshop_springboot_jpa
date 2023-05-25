2cbb40ee8e6ad3636d49493022eedda457ffdb10
Gui089 <guilhermesilba811@gmail.com>
2023-05-25 08:13:07 -0300

    Subtotal and total methods

--- src/main/java/com/project/course/entities/Payment.java
+++ src/main/java/com/project/course/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.project.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,6 +18,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
