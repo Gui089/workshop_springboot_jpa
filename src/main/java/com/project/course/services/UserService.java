5a62170e482fe9d19b55039f31df673bdd994b5d
Gui089 <guilhermesilba811@gmail.com>
2023-05-26 09:37:44 -0300

    Excepetion handler - findById

--- src/main/java/com/project/course/services/UserService.java
+++ src/main/java/com/project/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.project.course.services;
 
 import com.project.course.entities.User;
 import com.project.course.repositories.UserRepository;
+import com.project.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
        Optional<User> obj = repository.findById(id);
-       return obj.get();
+       return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
